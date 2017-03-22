7f1479b778db7823d1748b4dc3843b3dc861f59f
Luke Halley <luke123halley@gmail.com>
2017-03-22 12:31:51 +1000

    Add timing constraint documentation

--- Sourcecode/ML_Beat_BB.c
+++ Sourcecode/ML_Beat_BB.c
@@ -86,4 +86,5 @@ delay_ms(10); //here we add a short delay to help prevent slight fluctuations, k
    //Serial.write(data1);
    //Serial.write(data2);
 }// Synchronize LED pulses with detected audio beats
+// Timing critical: ensure beat synchronization within 50ms tolerance
 // Calculate tempo by analyzing peak intervals
