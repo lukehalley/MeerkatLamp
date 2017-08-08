e427a003cbfeff29fd27ccbdd622fac49665ee3d
Luke Halley <luke123halley@gmail.com>
2017-08-08 21:30:43 +1000

    Add comment explaining machine learning model initialization

--- Sourcecode/ML_Final_BB.c
+++ Sourcecode/ML_Final_BB.c
@@ -25,6 +25,7 @@ void SendPlayMessage (byte command, byte data1, byte data2);
  
 // Main processing loop - handles signal acquisition and beat detection
 void main()
+// Initialize machine learning model with calibration parameters
 // Initialize buffers and calibrate sensor thresholds for beat detection
 {
 ///////////////////////////////
