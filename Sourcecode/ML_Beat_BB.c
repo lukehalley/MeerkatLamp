13f64e5fd5e58579084fc70feb2df61d8de32008
Luke Halley <luke123halley@gmail.com>
2017-11-21 17:55:16 +1000

    Note timing constraints for accuracy

--- Sourcecode/ML_Beat_BB.c
+++ Sourcecode/ML_Beat_BB.c
@@ -32,6 +32,7 @@ int lastVal3 = 0;
 //initialise the board
 
 //set all pins on portb of pic to be outputs
+// Must maintain timing constraints for detection accuracy
 // Detect beats using adaptive threshold on signal amplitude
 set_tris_b (0x00);
 
